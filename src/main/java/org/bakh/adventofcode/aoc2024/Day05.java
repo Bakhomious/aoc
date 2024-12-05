bc02e104c2916d2f0b50bff15d378ad5be36e839
Bakhomious <bakhomioussvp@gmail.com>
2024-12-05 10:25:14 +0100

    WIP: Day 5 solution.

--- src/main/java/org/bakh/adventofcode/aoc2024/Day05.java
+++ src/main/java/org/bakh/adventofcode/aoc2024/Day05.java
@@ -2,7 +2,15 @@ package org.bakh.adventofcode.aoc2024;
 
 import org.bakh.adventofcode.Day;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Day05 extends Day {
+
+    private static final String PAGES_SEPARATOR = "\\|";
+    private static final String DATA_SEPARATOR = "";
 
     public Day05(String fileName) {
         super(fileName);
@@ -10,6 +18,29 @@ public class Day05 extends Day {
 
     @Override
     public String runPartOne() {
+        final var separator = getData().indexOf(DATA_SEPARATOR);
+
+        // Key: pages needed to be printed before
+        // Value: pages needed to be printed after
+        final var pages = getData().subList(0, separator).stream()
+            .map(page -> page.split(PAGES_SEPARATOR))
+            .collect(
+                Collectors.toMap(
+                    tokens -> Integer.parseInt(tokens[0]),
+                    tokens -> {
+                        final var values = new ArrayList<Integer>();
+                        values.add(Integer.parseInt(tokens[1]));
+                        return values;
+                    },
+                    (valuesList, duplicateKeyList) -> {
+                        valuesList.addAll(duplicateKeyList);
+                        return valuesList;
+                    }
+                )
+            );
+
+        System.out.println(pages);
+
         return "";
     }
 
